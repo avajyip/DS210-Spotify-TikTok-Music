0a84eeed46feafdb0f213e9aaf7937c8bde3a10a
Ava Yip <avajyip@gmail.com>
2024-04-30 19:55:21 -0400

    all files imported, separate modules made

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::fs::File;
 use std::io::prelude::*;
-use rand::Rng;
+mod song;
+use song::*;
 
-fn read_file(path: &str) -> Vec<(String, f64, f64, f64, f64, f64)> {
+fn read_tiktok_file(path: &str) -> Vec<(String, f64, f64, f64, f64, f64)> {
     let mut result: Vec<(String, f64, f64, f64, f64, f64)> = Vec::new();
     let file = File::open(path).expect("Could not open file");
     let buf_reader = std::io::BufReader::new(file).lines();
@@ -26,97 +27,41 @@ fn read_file(path: &str) -> Vec<(String, f64, f64, f64, f64, f64)> {
     return result;
 }
 
-fn create_nodes(vals: &Vec<(String, f64, f64, f64, f64, f64)>) -> Vec<Vec<f64>> {
-    let mut result: Vec<Vec<f64>> = Vec::new();
-    for val in vals {
-        let mut coordinates: Vec<f64> = Vec::new();
-        coordinates.push(val.1);
-        coordinates.push(val.2);
-        coordinates.push(val.3);
-        coordinates.push(val.4);
-        coordinates.push(val.5);
-        result.push(coordinates);
-    }
-    return result;
-}
+fn read_spotify_file(path: &str) -> Vec<(String, f64, f64, f64, f64, f64)> {
+    let mut result: Vec<(String, f64, f64, f64, f64, f64)> = Vec::new();
+    let file = File::open(path).expect("Could not open file");
+    let buf_reader = std::io::BufReader::new(file).lines();
 
-fn tracklist(vals: &Vec<(String, f64, f64, f64, f64, f64)>) -> Vec<String> {
-    let mut result: Vec<String> = Vec::new();
-    for val in vals {
-        result.push(val.0.clone());
-    }
-    return result;
-}
-
-fn select_random_sample(pts: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
-    let n = pts.len();
-    let mut result: Vec<Vec<f64>> = Vec::new();
-    let mut indices: Vec<usize> = Vec::new();
-    let mut count = 0;
-    while count < 50 {
-        let x = rand::thread_rng().gen_range(0..n);
-        if !indices.contains(&x) {
-            indices.push(x);
-            count += 1;
+    for (i, line) in buf_reader.enumerate() {
+        if i == 0 {
+            continue;
+        } else {
+            let line_str = line.expect("Error reading");
+            let v: Vec<&str> = line_str.trim().split('\t').collect();
+            let name = v[1].parse::<String>().unwrap();
+            let danceability = v[6].parse::<f64>().unwrap();
+            let energy = v[7].parse::<f64>().unwrap();
+            let loudness = v[9].parse::<f64>().unwrap();
+            let valence = v[15].parse::<f64>().unwrap();
+            let tempo = v[16].parse::<f64>().unwrap();
+            
+            result.push((name, danceability, energy, loudness, valence, tempo));
         }
     }
-    for i in indices {
-        result.push(pts[i].clone());
-    }
     return result;
-}
-
-fn distance(node1: Vec<f64>, node2: Vec<f64>) -> f64 {
-    let mut squared_distance: f64 = 0.0;
-    let n = node1.len();
-    for i in 0..n {
-        let x = node1[i] - node2[i];
-        squared_distance += x.powf(2.0);
-    }
-    return squared_distance.sqrt();
-}
-
-fn average_distance(pts: &Vec<Vec<f64>>) -> f64 {
-    let mut sum = 0.0;
-    let n = pts.len();
-    let num_pairings = (n * (n-1)) / 2;
-    for i in 0..n {
-        for j in (i+1)..n {
-            sum += distance(pts[i].to_vec(), pts[j].to_vec());
-        }
-    }
-    return sum/(num_pairings as f64);
 }
 
 fn main() {
-    let data = read_file("TikTok_songs_2019.tsv");
-    let nodes = create_nodes(&data);
-    let _tracklist = tracklist(&data);
+    let tiktok2019 = read_tiktok_file("TikTok_songs_2019.tsv");
+    let tiktok2020 = read_tiktok_file("TikTok_songs_2020.tsv");
+    let tiktok2021 = read_tiktok_file("TikTok_songs_2021.tsv");
+    let tiktok2022 = read_tiktok_file("TikTok_songs_2022.tsv");
+    let spotify = read_spotify_file("Spotify_top_charts.tsv");
+
+    let nodes = create_nodes(&tiktok2019);
+    let _tracklist = tracklist(&tiktok2019);
     let sample = select_random_sample(&nodes);
     let avg = average_distance(&sample);
 
-    /*println!("ALL:");
-    for node1 in &nodes {
-        for node2 in &nodes {
-            if node1 == node2 {
-                continue;
-            } else {
-                let dist = distance((node1).to_vec(), (node2).to_vec());
-                println!("{}", dist);
-            }
-        }
-    }
-
-    println!("SAMPLE:");
-    for node1 in &sample {
-        for node2 in &sample {
-            if node1 == node2 {
-                continue;
-            } else {
-                let dist = distance((node1).to_vec(), (node2).to_vec());
-                println!("{}", dist);
-            }
-        }
-    }*/
-    println!("{}", avg);
+    println!("{}", avg); 
 }
--- target/debug/deps/code-5d16c965d01e6c24.d
+++ target/debug/deps/code-5d16c965d01e6c24.d
@@ -1,5 +1,6 @@
-/Users/avayip/Downloads/DS210/DS210FINALPROJECT/DS210-Spotify-TikTok-Music/code/target/debug/deps/code-5d16c965d01e6c24: src/main.rs
+/Users/avayip/Downloads/DS210/DS210FINALPROJECT/DS210-Spotify-TikTok-Music/code/target/debug/deps/code-5d16c965d01e6c24: src/main.rs src/song.rs
 
-/Users/avayip/Downloads/DS210/DS210FINALPROJECT/DS210-Spotify-TikTok-Music/code/target/debug/deps/code-5d16c965d01e6c24.d: src/main.rs
+/Users/avayip/Downloads/DS210/DS210FINALPROJECT/DS210-Spotify-TikTok-Music/code/target/debug/deps/code-5d16c965d01e6c24.d: src/main.rs src/song.rs
 
 src/main.rs:
+src/song.rs:
