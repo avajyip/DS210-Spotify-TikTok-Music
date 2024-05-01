63bc8ec92072367ba16627213d06afbd79485014
Ava Yip <avajyip@gmail.com>
2024-04-30 23:41:52 -0400

    found avg and max distance for each dataset

--- src/main.rs
+++ src/main.rs
@@ -91,12 +91,12 @@ fn get_song_title(songs: &Vec<Song>, attributes: &Vec<f64>) -> String {
     return "NA".to_string();
 }
 
-fn select_random_sample(pts: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
+fn select_random_sample(pts: &Vec<Vec<f64>>, num_samples: &usize) -> Vec<Vec<f64>> {
     let n = pts.len();
     let mut result: Vec<Vec<f64>> = Vec::new();
     let mut indices: Vec<usize> = Vec::new();
     let mut count = 0;
-    while count < 50 {
+    while count < *num_samples {
         let x = rand::thread_rng().gen_range(0..n);
         if !indices.contains(&x) {
             indices.push(x);
@@ -151,18 +151,61 @@ fn max_distance(pts: &Vec<Vec<f64>>) -> (f64, Vec<f64>, Vec<f64>) {
 
 fn main() {
     let tiktok2019 = read_tiktok_file("TikTok_songs_2019.tsv");
-    //let tiktok2020 = read_tiktok_file("TikTok_songs_2020.tsv");
-    //let tiktok2021 = read_tiktok_file("TikTok_songs_2021.tsv");
-    //let tiktok2022 = read_tiktok_file("TikTok_songs_2022.tsv");
-    //let spotify = read_spotify_file("Spotify_top_charts.tsv");
+    let tiktok2020 = read_tiktok_file("TikTok_songs_2020.tsv");
+    let tiktok2021 = read_tiktok_file("TikTok_songs_2021.tsv");
+    let tiktok2022 = read_tiktok_file("TikTok_songs_2022.tsv");
+    let spotify = read_spotify_file("Spotify_top_charts.tsv");
 
-    let nodes = create_attribute_nodes(&tiktok2019);
-    let _tracklist = get_tracklist(&tiktok2019);
-    let sample = select_random_sample(&nodes);
-    let avg = average_distance(&sample);
-    let max = max_distance(&nodes);
-    let song1 = get_song_title(&tiktok2019, &max.1);
+    let nodes19 = create_attribute_nodes(&tiktok2019);
+    let sample19 = select_random_sample(&nodes19, &50);
+    let avg19 = average_distance(&sample19);
+    let (max_dist19, i19, j19) = max_distance(&nodes19);
+    println!("2019 TIKTOK
+            \navg distance: {}
+            \nmax distance: {}
+            \nsongs: {}; {}", 
+            avg19, max_dist19, get_song_title(&tiktok2019, &i19), get_song_title(&tiktok2019, &j19));
 
-    println!("{:?}", max);
-    println!("{:?}", song1); 
+    let nodes20 = create_attribute_nodes(&tiktok2020);
+    let sample20 = select_random_sample(&nodes20, &50);
+    let avg20 = average_distance(&sample20);
+    let (max_dist20, i20, j20) = max_distance(&nodes20);
+    println!("\n2020 TIKTOK
+            \navg distance: {}
+            \nmax distance: {}
+            \nsongs: {}; {}",
+            avg20, max_dist20, get_song_title(&tiktok2020, &i20), get_song_title(&tiktok2020, &j20));
+
+    let nodes21 = create_attribute_nodes(&tiktok2021);
+    let sample21 = select_random_sample(&nodes21, &50);
+    let avg21 = average_distance(&sample21);
+    let (max_dist21, i21, j21) = max_distance(&nodes21);
+    println!("\n2021 TIKTOK
+            \navg distance: {}
+            \nmax distance: {}
+            \nsongs: {}; {}",
+            avg21, max_dist21, get_song_title(&tiktok2021, &i21), get_song_title(&tiktok2021, &j21));
+
+    let nodes22 = create_attribute_nodes(&tiktok2022);
+    let sample22 = select_random_sample(&nodes22, &50);
+    let avg22 = average_distance(&sample22);
+    let (max_dist22, i22, j22) = max_distance(&nodes22);
+    println!("\n2022 TIKTOK
+            \navg distance: {}
+            \nmax distance: {}
+            \nsongs: {}; {}",
+            avg22, max_dist22, get_song_title(&tiktok2022, &i22), get_song_title(&tiktok2022, &j22));
+
+    let nodes_spotify = create_attribute_nodes(&spotify);
+    let sample_spotify = select_random_sample(&nodes_spotify, &200);
+    let avg_spotify = average_distance(&sample_spotify);
+    let (max_dist_spotify, i_spotify, j_spotify) = max_distance(&nodes_spotify);
+    println!("\n2000-2019 SPOTIFY
+            \navg distance: {}
+            \nmax distance: {}
+            \nsongs: {}; {}",
+            avg_spotify, max_dist_spotify, get_song_title(&spotify, &i_spotify), get_song_title(&spotify, &j_spotify));
+
+    let tiktok_total_avg = (avg19 + avg20 + avg21 + avg22)/4.0;
+    println!("tiktok avg: {}\nspotify avg: {}", tiktok_total_avg, avg_spotify);
 }
