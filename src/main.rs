1c111901f8efb8049181cdf96fad74c9a527b987
Ava Yip <avajyip@gmail.com>
2024-04-30 22:17:27 -0400

    created Song struct, made module split more logical

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::fs::File;
 use std::io::prelude::*;
+use rand::Rng;
 mod song;
 use song::*;
 
-fn read_tiktok_file(path: &str) -> Vec<(String, f64, f64, f64, f64, f64)> {
-    let mut result: Vec<(String, f64, f64, f64, f64, f64)> = Vec::new();
+fn read_tiktok_file(path: &str) -> Vec<Song> {
+    let mut result: Vec<Song> = Vec::new();
     let file = File::open(path).expect("Could not open file");
     let buf_reader = std::io::BufReader::new(file).lines();
 
@@ -21,14 +22,14 @@ fn read_tiktok_file(path: &str) -> Vec<(String, f64, f64, f64, f64, f64)> {
             let valence = v[14].parse::<f64>().unwrap();
             let tempo = v[15].parse::<f64>().unwrap();
             
-            result.push((name, danceability, energy, loudness, valence, tempo));
+            result.push(Song::create_song(&name, &danceability, &energy, &loudness, &valence, &tempo));
         }
     }
     return result;
 }
 
-fn read_spotify_file(path: &str) -> Vec<(String, f64, f64, f64, f64, f64)> {
-    let mut result: Vec<(String, f64, f64, f64, f64, f64)> = Vec::new();
+fn read_spotify_file(path: &str) -> Vec<Song> {
+    let mut result: Vec<Song> = Vec::new();
     let file = File::open(path).expect("Could not open file");
     let buf_reader = std::io::BufReader::new(file).lines();
 
@@ -45,10 +46,72 @@ fn read_spotify_file(path: &str) -> Vec<(String, f64, f64, f64, f64, f64)> {
             let valence = v[15].parse::<f64>().unwrap();
             let tempo = v[16].parse::<f64>().unwrap();
             
-            result.push((name, danceability, energy, loudness, valence, tempo));
+            result.push(Song::create_song(&name, &danceability, &energy, &loudness, &valence, &tempo));
         }
     }
     return result;
+}
+
+fn create_attribute_nodes(songs: &Vec<Song>) -> Vec<Vec<f64>> {
+    let mut result: Vec<Vec<f64>> = Vec::new();
+    for song in songs {
+        let mut coordinates: Vec<f64> = Vec::new();
+        coordinates.push(song.danceability);
+        coordinates.push(song.energy);
+        coordinates.push(song.loudness);
+        coordinates.push(song.valence);
+        coordinates.push(song.tempo);
+        result.push(coordinates);
+    }
+    return result;
+}
+
+fn tracklist(songs: &Vec<Song>) -> Vec<String> {
+    let mut result: Vec<String> = Vec::new();
+    for song in songs {
+        result.push(song.title.clone());
+    }
+    return result;
+}
+
+fn select_random_sample(pts: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
+    let n = pts.len();
+    let mut result: Vec<Vec<f64>> = Vec::new();
+    let mut indices: Vec<usize> = Vec::new();
+    let mut count = 0;
+    while count < 50 {
+        let x = rand::thread_rng().gen_range(0..n);
+        if !indices.contains(&x) {
+            indices.push(x);
+            count += 1;
+        }
+    }
+    for i in indices {
+        result.push(pts[i].clone());
+    }
+    return result;
+}
+
+fn distance(node1: Vec<f64>, node2: Vec<f64>) -> f64 {
+    let mut squared_distance: f64 = 0.0;
+    let n = node1.len();
+    for i in 0..n {
+        let x = node1[i] - node2[i];
+        squared_distance += x.powf(2.0);
+    }
+    return squared_distance.sqrt();
+}
+
+fn average_distance(pts: &Vec<Vec<f64>>) -> f64 {
+    let mut sum = 0.0;
+    let n = pts.len();
+    let num_pairings = (n * (n-1)) / 2;
+    for i in 0..n {
+        for j in (i+1)..n {
+            sum += distance(pts[i].to_vec(), pts[j].to_vec());
+        }
+    }
+    return sum/(num_pairings as f64);
 }
 
 fn main() {
@@ -58,7 +121,7 @@ fn main() {
     let tiktok2022 = read_tiktok_file("TikTok_songs_2022.tsv");
     let spotify = read_spotify_file("Spotify_top_charts.tsv");
 
-    let nodes = create_nodes(&tiktok2019);
+    let nodes = create_attribute_nodes(&tiktok2019);
     let _tracklist = tracklist(&tiktok2019);
     let sample = select_random_sample(&nodes);
     let avg = average_distance(&sample);
--- src/song.rs
+++ src/song.rs
@@ -1,63 +1,14 @@
-use rand::Rng;
-
-pub fn create_nodes(vals: &Vec<(String, f64, f64, f64, f64, f64)>) -> Vec<Vec<f64>> {
-    let mut result: Vec<Vec<f64>> = Vec::new();
-    for val in vals {
-        let mut coordinates: Vec<f64> = Vec::new();
-        coordinates.push(val.1);
-        coordinates.push(val.2);
-        coordinates.push(val.3);
-        coordinates.push(val.4);
-        coordinates.push(val.5);
-        result.push(coordinates);
-    }
-    return result;
+pub struct Song {
+    pub title: String,
+    pub danceability: f64,
+    pub energy: f64,
+    pub loudness: f64,
+    pub valence: f64,
+    pub tempo: f64,
 }
 
-pub fn tracklist(vals: &Vec<(String, f64, f64, f64, f64, f64)>) -> Vec<String> {
-    let mut result: Vec<String> = Vec::new();
-    for val in vals {
-        result.push(val.0.clone());
+impl Song {
+    pub fn create_song(ti: &String, d: &f64, e: &f64, l: &f64, v: &f64, te: &f64) -> Song {
+        return Song{title: ti.clone(), danceability: *d, energy: *e, loudness: *l, valence: *v, tempo: *te};
     }
-    return result;
-}
-
-pub fn select_random_sample(pts: &Vec<Vec<f64>>) -> Vec<Vec<f64>> {
-    let n = pts.len();
-    let mut result: Vec<Vec<f64>> = Vec::new();
-    let mut indices: Vec<usize> = Vec::new();
-    let mut count = 0;
-    while count < 50 {
-        let x = rand::thread_rng().gen_range(0..n);
-        if !indices.contains(&x) {
-            indices.push(x);
-            count += 1;
-        }
-    }
-    for i in indices {
-        result.push(pts[i].clone());
-    }
-    return result;
-}
-
-pub fn distance(node1: Vec<f64>, node2: Vec<f64>) -> f64 {
-    let mut squared_distance: f64 = 0.0;
-    let n = node1.len();
-    for i in 0..n {
-        let x = node1[i] - node2[i];
-        squared_distance += x.powf(2.0);
-    }
-    return squared_distance.sqrt();
-}
-
-pub fn average_distance(pts: &Vec<Vec<f64>>) -> f64 {
-    let mut sum = 0.0;
-    let n = pts.len();
-    let num_pairings = (n * (n-1)) / 2;
-    for i in 0..n {
-        for j in (i+1)..n {
-            sum += distance(pts[i].to_vec(), pts[j].to_vec());
-        }
-    }
-    return sum/(num_pairings as f64);
 }
